966ccad4df2d5b24ae64f5ede4ed352917227845
got5102 <5102got@gmail.com>
2025-05-04 04:57:48 +0900

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,4 @@
 const { Pool } = require('pg');
-
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -7,7 +6,4 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
 });
-
-module.exports = {
-  pool
-};
+module.exports = pool;
