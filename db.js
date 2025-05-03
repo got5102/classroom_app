4fb113d767a542467093c290d7715a02792f0456
got5102 <5102got@gmail.com>
2025-05-04 04:30:37 +0900

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,10 @@
-// db.js
-import { Pool } from 'pg';
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
 });
 
-/**
- * SQL クエリを実行します。
- * @param {string} text - SQL テキスト
- * @param {any[]} params - パラメータ配列
- * @returns {Promise<import('pg').QueryResult<any>>}
- */
-export async function query(text, params) {
-  return pool.query(text, params);
-}
+module.exports = {
+  query: (text, params) => pool.query(text, params)
+};
